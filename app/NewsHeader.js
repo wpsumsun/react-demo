1b7b612c9a2597164a364045fc9485faf20471e4
Summer <wpsumsun@126.com>
2017-05-02 18:11:13 +0800

    react-demo

---
+++ app/NewsHeader.js
@@ -0,0 +1,78 @@
+import React,{Component} from 'react';
+import imageLogo from './y18.gif';
+import './NewsHeader.css';
+
+export default class NewsHeader extends Component {
+  render() {
+    return (
+        <div className="newsHeader">
+              {this.getLogo()}
+              {this.getTitle()}
+              {this.getNav()}
+              {this.getlogin()}
+        </div>
+        );
+  }
+  getLogo(){
+      return (
+          <div className="newsHeader-logo">
+              <a href="https://news.ycombinator.com"><img src={imageLogo}/></a>
+          </div>
+      );
+  }
+  getTitle(){
+      return (
+          <div className="newsHeader-title">
+              <a className="newsHeader-link" href="https://news.ycombinator.com/" >Hacker News</a>
+          </div>
+      )
+  }
+  getNav(){
+      var navLinks=[
+          {
+              name:'new',
+              url:'newest'
+          },
+          {
+              name:'comments',
+              url:'newcomments'
+          },
+          {
+              name:'show',
+              url:'show'
+          },
+          {
+              name:'ask',
+              url:'ask'
+          },
+          {
+              name:'jobs',
+              url:'jobs'
+          },
+          {
+              name:'submit',
+              url:'submit'
+          },
+      ];
+      return (
+          <div className="newsHeader-nav">
+              {
+                  navLinks.map(function(navLink){
+                      return(
+                          <a key={navLink.url} className="newsHeader-navLink NewsHeader-textLink" href={"https://news.ycombinator.com/"+navLink.url}>
+                            {navLink.name}
+                          </a>
+                      )
+                  })
+              }
+          </div>
+      );
+  }
+  getlogin(){
+      return (
+          <div className="newsHeader-login">
+              <a className="newsHeader-textLink" href="https://news.ycombinator.com/login?goto=news">login</a>
+          </div>
+      );
+  }
+}
---
+++ app/NewsList.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import NewsHeader from './NewsHeader.js';
+import NewsItem from './NewsItem.js';
+
+export default class NewsList extends React.Component {
+  render() {
+    return (
+        <div className="newsList">
+          <NewsHeader />
+          <div className="newsList-newsItem">
+            {
+              (this.props.items).map(function(item, index) {
+                return (
+                    <NewsItem key={item.id} item={item} rank={index+1} />
+                    );
+              })
+            }
+          </div>
+        </div>
+        );
+  }
+   /*render() {
+    return (
+        <div className="newsList">
+          <NewsHeader />
+          <div className="newsList-newsItem">
+            {
+              (this.props.items).map(function(item, index) {
+                return (
+                    <NewsItem key={item.id} item={item} rank={index+1} />
+                    );
+              })
+            }
+          </div>
+        </div>
+        );
+  }*/
+}
